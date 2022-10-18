67f107e3084973e510ac79d9f49ad6b792c5e364
deltafolk <115680569+deltafolk@users.noreply.github.com>
2022-10-18 21:29:41 +0700

    update port to 3000

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 
-app.listen(8080,()=>{
-    console.log('running on port 8080');
+app.listen(3000,()=>{
+    console.log('running on port 3000');
 })
